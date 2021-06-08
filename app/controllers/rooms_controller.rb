48e99815ca318168cd3cd33498bc523e0c505211
Hajimojikun <ichig9.xx@gmail.com>
2021-06-08 23:18:45 +0900

    roomのindexファイルとshowファイルの編集

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,28 @@
 class RoomsController < ApplicationController
 
   def index
+    @rooms = Room.all
+  end
+
+  def new
+    @rooms = Room.new
+  end
+
+  def create
+    @rooms = Room.new(rooms_params)
+     if @rooms.save
+      redirect_to root_path
+     else
+      render :new
+     end
+  end
+
+  def show
+    @rooms = Room.find(params[:id])
   end
   
+  private
+  def rooms_params
+    params.require(:room).permit(:title, :description).merge(user_id: current_user.id)
+  end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,6 @@
 class Room < ApplicationRecord
+  with_options presence: true do
+  validates :title
+  validates :description
+  end 
 end
